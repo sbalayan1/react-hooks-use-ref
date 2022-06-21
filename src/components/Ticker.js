8a1e0d76edd4d973f5fc47efbaed3c79fafd3490
sbalayan1 <balayans2014@yahoo.com>
2022-06-21 16:32:58 -0700

    update

--- src/components/Ticker.js
+++ src/components/Ticker.js
@@ -1,22 +1,37 @@
-import React, { useEffect, useState } from "react";
 import { makeRandomNumber } from "../utils";
+import {useRef, useEffect, useState} from 'react'
 
 function Ticker() {
-  const [price, setPrice] = useState(0);
-  const [color, setColor] = useState("black");
+  const [price, setPrice] = useState(0)
+  const [color, setColor] = useState('black')
+  const prevPrice = useRef(price)
 
   useEffect(() => {
-    const id = setInterval(() => setPrice(makeRandomNumber), 1000);
-    return function () {
-      clearInterval(id);
-    };
-  }, []);
+    let priceAction = setInterval(() => {
+      let randomNumber = makeRandomNumber(price)
+      setPrice(randomNumber)
+
+      if (randomNumber > prevPrice.current) {
+        setColor('green')
+      } else if (randomNumber < prevPrice.current) {
+        setColor('red')
+      } else {
+        setColor('black')
+      }
+  
+      prevPrice.current = randomNumber
+    }, 1000)
+
+    return function cleanup() {
+      clearInterval(priceAction)
+    }
+  }, [])
 
   return (
-    <div>
-      <h1>TickerMaster</h1>
-      <h2 style={{ color: color }}>Price: ${price}</h2>
-    </div>
+      <div>
+          <h1>TickerMaster</h1>
+          <h2 style={{ color: color }}>Price: ${price}</h2>
+      </div>
   );
 }
 
